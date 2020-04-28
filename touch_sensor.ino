5899c965ac2750b868d468fa44b759e6fb5d2296
muhammedalys <muhammed_aly_ozen@hotmail.com>
2020-04-28 20:25:09 +0300

    Add files via upload

---
+++ touch_sensor.ino
@@ -0,0 +1,19 @@
+#define TouchSensor 2
+
+int relay = 8;
+
+boolean currentState = LOW; boolean lastState = LOW; boolean RelayState = LOW;
+
+void setup() { Serial.begin(9600); pinMode(relay, OUTPUT);
+pinMode(TouchSensor, INPUT); }
+
+void loop() { currentState = digitalRead(TouchSensor); if (currentState == HIGH && lastState == LOW){ Serial.println("pressed"); delay(1);
+
+if (RelayState == HIGH){
+  digitalWrite(relay, LOW);
+  RelayState = LOW;
+} else {
+  digitalWrite(relay, HIGH);
+  RelayState = HIGH;
+}
+} lastState = currentState; }
